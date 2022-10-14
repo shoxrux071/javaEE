215bb0772a60aed2ee7e9c2f4dcb303ea46d8984
shoxrux071 <begratefuller071@gmail.com>
2022-10-14 21:44:39 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    int a = 2+2;
+}
---
+++ src/main/java/org/example/controller/SignUp.java
@@ -0,0 +1,19 @@
+package org.example.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUp extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.sendRedirect("register.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
